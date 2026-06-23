02d936ff907d927493ae26058780059a6db5b47c
Letícia <leticiabsouza09@gmail.com>
2026-06-22 23:06:55 -0300

    Configura app para rodar com Waitress no Windows e Gunicorn no Render

--- app.py
+++ app.py
@@ -8,5 +8,9 @@ def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    if os.name == "nt":  # Windows
+        from waitress import serve
+        serve(app, host="0.0.0.0", port=5000)
+    else:  # Linux (Render)
+        port = int(os.environ.get("PORT", 5000))
+        app.run(host="0.0.0.0", port=port)
