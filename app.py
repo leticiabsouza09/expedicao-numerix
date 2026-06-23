8f1252668fb372fe4719cb2ac12b3ab078d93684
Letícia <leticiabsouza09@gmail.com>
2026-06-22 23:22:58 -0300

    Adiciona favicon personalizado e ajustes no app

--- app.py
+++ app.py
@@ -10,7 +10,9 @@ def index():
 if __name__ == "__main__":
     if os.name == "nt":  # Windows
         from waitress import serve
+        print("Servidor rodando com Waitress em http://127.0.0.1:5000")
         serve(app, host="0.0.0.0", port=5000)
     else:  # Linux (Render)
         port = int(os.environ.get("PORT", 5000))
+        print(f"Servidor rodando em porta {port}")
         app.run(host="0.0.0.0", port=port)
